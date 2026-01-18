5010bf8c76af6785631f3b4c5468f8759fde8a81
Matheus Cardoso <matheus@cardo.so>
2026-01-18 09:55:45 -0300

    chore: add lib/ast [skip ci]

--- apps/meteor/app/slackbridge/server/SlackAdapter.ts
+++ apps/meteor/app/slackbridge/server/SlackAdapter.ts
@@ -29,6 +29,8 @@ import { executeSetReaction } from '../../reactions/server/setReaction';
 import { settings } from '../../settings/server';
 import { getUserAvatarURL } from '../../utils/server/getUserAvatarURL';
 
+const { App: SlackApp } = slackBolt;
+
 export default class SlackAdapter {
 	constructor(slackBridge) {
 		slackLogger.debug({ msg: 'constructor' });
--- apps/meteor/ee/server/startup/index.ts
+++ apps/meteor/ee/server/startup/index.ts
@@ -18,6 +18,6 @@ export const registerEEBroker = async (): Promise<void> => {
 		api.setBroker(startBroker());
 		await api.start();
 	} else {
-		require('./presence');
+		await import('./presence');
 	}
 };
